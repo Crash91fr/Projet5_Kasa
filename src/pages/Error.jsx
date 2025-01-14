63e6adbd6f08415122d1d0aba0e35ddb1707b8c2
Jauffrey Stephan <jauffrey.stephan@gmail.com>
2025-01-14 15:48:48 +0100

    Modification niveau de heading,

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -7,8 +7,8 @@ const Error = () => {
     return (
         <>
             <div className="error">
-                <h1>404</h1>
-                <h2>Oups! La page que vous demandez n&apos;existe pas.</h2>
+                <h2>404</h2>
+                <h3>Oups! La page que vous demandez n&apos;existe pas.</h3>
                 <Link to="/">Retourner sur la page d&apos;accueil</Link>
             </div>
         </>
